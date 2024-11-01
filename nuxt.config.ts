70d863e25a75671fdae3a7e22b2b02cc383063f8
dante <jamesmidzi@gmail.com>
2024-11-01 12:25:01 +0200

    feat: add image, icon, font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  srcDir: "src/",
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
-  devtools: { enabled: true }
-})
+    compatibilityDate: "2024-04-03",
+    srcDir: "src/",
+    modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxt/image"],
+    devtools: { enabled: true },
+});
