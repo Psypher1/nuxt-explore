53156fdacea15d623bc1db14063987798f2ea028
dante <jamesmidzi@gmail.com>
2024-11-03 09:53:03 +0200

    navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+<template>
+    <header class="py-4 border-b">
+        <nav class="flex container items-center">
+            <a href="" class="flex gap-1 items-center">
+                <!-- <img class="w- 14" src="/images/icon.png" alt=" a green icon" /> -->
+                <span class="text-2xl font-semiSbold">Nuxtcipes</span>
+            </a>
+            <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+                <li>
+                    <a href="">Home</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
