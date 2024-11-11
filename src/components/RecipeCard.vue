6da3d4ea32b32fc0a9a09f2ec24b7893cca1a183
dante <jamesmidzi@gmail.com>
2024-11-11 14:23:13 +0200

    feat: exract recipte card

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+    recipe: Object,
+});
+</script>
+
+<template>
+    <article class="shadow-md rounded-md">
+        <NuxtImg
+            class="rounded-t-md"
+            format="png"
+            sizes="100vw sm:50vw"
+            densities="x1"
+            :src="recipe.image"
+            :alt="recipe.name"
+        />
+        <div class="p-4">
+            <h3 class="font-semibold text-lg md:text-xl lg:text-2xl">
+                {{ recipe.name }}
+            </h3>
+            <div class="flex items-center justify-around bg-white/80">
+                <p class="flex items-center gap-1">
+                    <Icon name="mdi:clock-outline" style="color: #f79f1a" />
+                    <span>{{ recipe.cookTimeMinutes }}</span>
+                </p>
+                <p class="flex items-center gap-1">
+                    <Icon name="mdi:fire" style="color: #f79f1a" />
+                    <span>{{ recipe.caloriesPerServing }}</span>
+                </p>
+                <p class="flex items-center gap-1">
+                    <Icon name="mdi:star" style="color: #f79f1a" />
+                    <span>{{ recipe.rating }} ({{ recipe.reviewCount }})</span>
+                </p>
+            </div>
+            <NuxtLink
+                :to="`/recipes/${recipe.id}`"
+                class="px-4 py-2 mt-4 inline-block text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
+            >
+                View
+            </NuxtLink>
+        </div>
+    </article>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -41,41 +41,7 @@ useSeoMeta({
         <h2 class="text-3xl lg:text-4xl font-semibold">Discover, Create, Share</h2>
         <p class="text-lg lg:text-xl mt-2">Check out our most popular recipes!</p>
         <div v-if="!error" class="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <article v-for="recipe in data?.recipes" :key="recipe.id" class="shadow-md rounded-md">
-                <NuxtImg
-                    class="rounded-t-md"
-                    format="png"
-                    sizes="100vw sm:50vw"
-                    densities="x1"
-                    :src="recipe.image"
-                    :alt="recipe.name"
-                />
-                <div class="p-4">
-                    <h3 class="font-semibold text-lg md:text-xl lg:text-2xl">
-                        {{ recipe.name }}
-                    </h3>
-                    <div class="flex items-center justify-around bg-white/80">
-                        <p class="flex items-center gap-1">
-                            <Icon name="mdi:clock-outline" style="color: #f79f1a" />
-                            <span>{{ recipe.cookTimeMinutes }}</span>
-                        </p>
-                        <p class="flex items-center gap-1">
-                            <Icon name="mdi:fire" style="color: #f79f1a" />
-                            <span>{{ recipe.caloriesPerServing }}</span>
-                        </p>
-                        <p class="flex items-center gap-1">
-                            <Icon name="mdi:star" style="color: #f79f1a" />
-                            <span>{{ recipe.rating }} ({{ recipe.reviewCount }})</span>
-                        </p>
-                    </div>
-                    <NuxtLink
-                        :to="`/recipes/${recipe.id}`"
-                        class="px-4 py-2 mt-4 inline-block text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
-                    >
-                        View
-                    </NuxtLink>
-                </div>
-            </article>
+            <RecipeCard v-for="recipe in data?.recipes" :recipe="recipe" />
         </div>
         <p v-else>Something went kapoot</p>
     </section>
